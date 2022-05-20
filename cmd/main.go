1d8e743b98653c65a2ffc51e7ccb13d2067e1337
Dominux <kir.gimranov@gmail.com>
2022-05-20 19:26:48 +0300

    Added server mode

--- cmd/main.go
+++ cmd/main.go
@@ -7,19 +7,28 @@ import (
 
 	tele "gopkg.in/telebot.v3"
 
-	artInfo "artTgBot/internal/info"
+	artInfo "artTgBot/internal/apps/info"
 )
 
 func main() {
-	pref := tele.Settings{
-		Token:  os.Getenv("TOKEN"),
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
-	}
 
-	b, err := tele.NewBot(pref)
-	if err != nil {
-		log.Fatal(err)
-		return
+	var b *tele.Bot
+	var err error
+	switch os.Getenv("MODE") {
+	case "PRODUCTION":
+		panic("Not implemented")
+
+	default:
+		pref := tele.Settings{
+			Token:  os.Getenv("API_TOKEN"),
+			Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		}
+
+		b, err = tele.NewBot(pref)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 	}
 
 	// Info handling
